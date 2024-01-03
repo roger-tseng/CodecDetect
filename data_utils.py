1e270b38d18919547ce2fd11e5c46c3212e6750f
hbwu-ntu <f07921092@ntu.edu.tw>
2024-01-03 19:30:40 +0800

    revise bugs in pad_random

--- data_utils.py
+++ data_utils.py
@@ -50,7 +50,7 @@ def pad(x, max_len=64600):
 def pad_random(x: np.ndarray, max_len: int = 64600):
     x_len = x.shape[0]
     # if duration is already long enough
-    if x_len >= max_len:
+    if x_len > max_len:
         stt = np.random.randint(x_len - max_len)
         return x[stt : stt + max_len]
 
